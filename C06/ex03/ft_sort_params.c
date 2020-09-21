82cca5822c6efc9e0fc495b64d44db091f8a7952
mathis <mathismartini1@gmail.com>
2020-09-21 23:51:21 +0200

    Les arguments du main

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: matmarti <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/17 18:40:18 by mmartini          #+#    #+#             */
+/*   Updated: 2020/09/17 19:44:31 by matmarti         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int		ft_strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while ((s1[i] || s2[i]) && (s1[i] == s2[i]))
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char *temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int		main(int ac, char **av)
+{
+	int i;
+	int j;
+
+	i = 1;
+	while (i < ac)
+	{
+		j = i + 1;
+		while (j != ac)
+		{
+			if (ft_strcmp(av[i], av[j]) > 0)
+				ft_swap(&av[i], &av[j]);
+			j++;
+		}
+		i++;
+	}
+	i = 1;
+	while (i != ac)
+	{
+		ft_putstr(av[i]);
+		ft_putstr("\n");
+		i++;
+	}
+}
